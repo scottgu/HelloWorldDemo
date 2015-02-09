64634af816462b9eaad7db374ed7f05f57cf41c1
Scott Guthrie <scott@guthriefamily.org>
2015-02-08 21:11:20 -0800

    updated text

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ var port = process.env.PORT || 1337;
 
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
+  res.end('Hello YCombinator!\n');
 }).listen(port);
