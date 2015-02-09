197fca90f7b76a77457c22c85f526826846d9b23
Scott Guthrie <scott@guthriefamily.org>
2015-02-08 21:15:10 -0800

    reset text

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ var port = process.env.PORT || 1337;
 
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello YCombinator!\n');
+  res.end('Hello World!\n');
 }).listen(port);
