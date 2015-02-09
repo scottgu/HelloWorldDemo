6fb8183e85ef9d7ff068f152c254fbd6cc25ac1a
Scott Guthrie <scott@guthriefamily.org>
2015-02-09 09:20:08 -0800

    updated to wrap html

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ var port = process.env.PORT || 1337;
 
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World!\n');
+  res.end('<html><body><h1>Hello World!</h1></body></html>\n');
 }).listen(port);
